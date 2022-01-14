d21fb5a4f6af6a23fb655d3409680992331d01fc
ahmed salah <asalah@silicon-mind.com>
2022-01-14 11:54:57 +0200

    change method update number for message

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,9 @@
 class Chat < ApplicationRecord
     belongs_to :application, counter_cache: true
     has_many :messages
+    after_create :add_to_redis_message_number
+
+    def add_to_redis_message_number
+        Redis.current.set("#{self.token}-#{self.number}", 0)
+    end
 end
