d910e6a7153543b48b0afa9ee9815e31df2eb4ef
ahmed salah <asalah@silicon-mind.com>
2022-01-15 03:30:13 +0200

    remove id of message entity

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,15 @@ class Message < ApplicationRecord
     belongs_to :chat , counter_cache: true
     include Searchable
     after_commit :index_message 
+
+    def as_json(options = {})
+        {
+            number: number,
+            text: text,
+            chat_id: chat_id,
+            created_at: created_at,
+            updated_at: updated_at
+
+        }
+    end
 end
