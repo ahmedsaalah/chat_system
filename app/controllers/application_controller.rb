1aef4ae66a0b5c28c71f4aeaaf38e33dafcbdd1e
ahmed salah <asalah@silicon-mind.com>
2022-01-16 00:48:47 +0200

    handling missing param

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,19 @@
 class ApplicationController < ActionController::API
     rescue_from ActiveRecord::RecordNotFound, with: :not_found
     rescue_from ActionController::RoutingError, with: :not_found
+    rescue_from ActionController::ParameterMissing , with: :unprocessable_entity
+
 
     def route_not_found
-        render json: {Error: 404, Message: "route not found."}, status: :not_found
+        render json: {Error: 404, detail: "route not found."}, status: :not_found
     end
 
     def not_found(e)
         render json: {  title: "Record not Found",
             status: 404,
             detail: "We could not find the object you were looking for.",}, status: :not_found
+    end
+    def unprocessable_entity(e)
+        render json: {  title: "Validation Error" , status: 422 , detail: e }, status: :unprocessable_entity
     end
 end
